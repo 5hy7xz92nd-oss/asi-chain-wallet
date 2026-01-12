7fdb522a766f935f11b04fb397c5ad4bf181705b
Marina Fedyantseva <marie.saez@mail.ru>
2026-01-12 12:50:32 +0300

    [AS-485] fix types of images

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,10 +1,5 @@
 import { AwsWafCaptcha, AwsWafIntegration } from "components/Captcha/types";
 
-declare module '*.png';
-declare module '*.jpg';
-declare module '*.jpeg';
-declare module '*.svg';
-
 declare global {
   interface Window {
     AwsWafIntegration: AwsWafIntegration;
